7a8ca8f751e356eaf5c66becfd1b7c3a0c00df1b
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-04-15 10:27:24 +0300

    !

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
-
-    function getInfo(){
-        fetch("http://64.225.74.130/api/paints/getstart")
+    let url="http://64.225.74.130";
+    let html="";
+        fetch(url+"/api/paints/getstart")
         .then((response)=>{
-            console.log(response.json)
+            return response.json()})
+            .then((json)=>{
+            for(let i=0; i<json.length-1; i++){
+            let image = url+json[i]["src"];
+            let author =json[i]["author"];
+            let title = json[i]["title"];
+            html+=`<div class="container" >
+            <div class="img"><img style="width:300px" src="${image}"/></div>
+            <div class="title">${title}</div>
+            <div class="author">${author}</div>
+            </div>`;
+            $(".container").css({width:"300px", heigh:"500px"});
+        }
+        $(".pictures").html(html);
         })
-    }
 })
+
