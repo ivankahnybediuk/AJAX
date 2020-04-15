9e3109b101084f7d0f730380dfe0ca59df3b0374
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-04-15 08:34:55 +0300

    1

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+$(document).ready(function(){
 
+    function getInfo(){
+        fetch("http://64.225.74.130/api/paints/getstart")
+        .then((response)=>{
+            console.log(response.json)
+        })
+    }
+})
